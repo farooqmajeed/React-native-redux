65c7a7c7aad48991d04c2124c3892639c6bc665d
farooqmajeed <farooq306@hotmail.com>
2017-07-01 15:33:54 +0500

    React native using redux

--- src/app.js
+++ src/app.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import reducers from './reducers'
+import reducers from './reducers';
+import { Header } from './components/common';
 
 const App = () => {
     return (
         <Provider store={createStore(reducers)}>
-        <View />
+            <View>
+                <Header headerText="TECH STACK" />
+                <Text> hello </Text>
+            </View>
         </Provider>
-        
+
     );
 };
 export default App;
